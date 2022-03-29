c8dcdf845beb202972c91b80752163073598b44f
jas0nc <33709736+jas0nc@users.noreply.github.com>
2022-03-29 15:58:41 +0800

    added esp01M gpio

--- sketch/thermostat/settings.h
+++ sketch/thermostat/settings.h
@@ -1,6 +1,6 @@
 /* GPIO Settings */
-#define IR_PIN             0 // ESP01S GPIO0
-#define DHT_PIN            2  // ESP01S GPIO2
+#define IR_PIN             4 // ESP01S GPIO0, ESP01M GPIO4
+#define DHT_PIN            0  // ESP01S GPIO2, ESP01M GPIO0
 #define DHT_TYPE           22  // DHT11 = 11, DHT22 = 22
 
 #define AC_MODE            3  // DAIKIN = 1, PANASONIC = 2, TOSHIBA = 3
